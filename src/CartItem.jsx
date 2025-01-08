6bf1282fff975aae421e5deba34c875425710127
diogo123310 <diogoludobini@gmail.com>
2025-01-08 17:02:50 +0000

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,8 +9,9 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
-        
-  };
+  // Iterar pelo array de itens no carrinho e somar o custo total
+  return cart.reduce((total, item) => total + item.quantity * item.cost, 0);
+};
 
   const handleContinueShopping = (e) => {
         
