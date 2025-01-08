4e51d7595813f8942bc68092bbc6e73493314475
diogo123310 <diogoludobini@gmail.com>
2025-01-08 08:22:59 -0500

    mudancas4

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import CartItem from './CartItem';
 
 export const CartSlice = createSlice({
   name: 'cart',
@@ -30,6 +31,6 @@ export const CartSlice = createSlice({
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
-
 export default CartSlice.reducer;
+
 
