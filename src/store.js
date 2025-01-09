b3354abf28d699f29f032c944b38eab4661153b0
diogo123310 <diogoludobini@gmail.com>
2025-01-09 06:07:43 -0500

    bugsfixed

--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
+
 const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+  reducer: {
+    cart: cartReducer, // Attach cartReducer to the store
+  },
 });
-export default store
+
+export default store;
+
